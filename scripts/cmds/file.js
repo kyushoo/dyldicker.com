9b9c3a62b38d3ce783b3c282acd7176323b899ce
kyletekser04ph <kivlynx42@gmail.com>
2024-09-29 19:42:24 +0800

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -4,7 +4,7 @@ module.exports = {
   config: {
     name: "file",
     version: "1.0",
-    author: "Elohime hataku",
+    author: "kylepogi",
     countDown: null,
     role: 2,
     shortDescription: "Send bot script",
@@ -15,14 +15,14 @@ module.exports = {
 
   onStart: async function ({ message, args, api, event }) {
     const fileName = args[0];
-    const permission = ["100088286122703"];
+    const permission = ["61566232924755"];
     if (!fileName) {
-      return api.sendMessage("Please provide a file name.", event.threadID, event.messageID);
+      return api.sendMessage("ℹ️ 𝗣𝗹𝗲𝗮𝘀𝗲 input a valid file name.", event.threadID, event.messageID);
     }
 
     const filePath = __dirname + `/${fileName}.js`;
     if (!fs.existsSync(filePath)) {
-      return api.sendMessage(`File not found: ${fileName}.js`, event.threadID, event.messageID);
+      return api.sendMessage(`⚠️ 𝗔𝗖𝗖𝗘𝗦𝗦 𝗗𝗘𝗡𝗜𝗘𝗗.\n\nFile not found: ${fileName}.js`, event.threadID, event.messageID);
     }
 
     const fileContent = fs.readFileSync(filePath, 'utf8');
